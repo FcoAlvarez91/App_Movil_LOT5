da56bb7663cb7b216e9e569d59695e07edf10d98
Ignacio Carril <iacarril@miuandes.cl>
2018-07-03 03:27:28 -0400

    monster views and data

---
+++ app/src/main/java/montero/app_movil_lot5/MonsterAdapter.java
@@ -0,0 +1,50 @@
+package montero.app_movil_lot5;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import montero.app_movil_lot5.Models.Monster;
+
+public class MonsterAdapter extends ArrayAdapter<Monster> {
+
+    private Context mContext;
+    private List<Monster> MonsterList;
+
+    public MonsterAdapter(Context context, ArrayList<Monster> list) {
+        super(context, 0, list);
+        mContext = context;
+        MonsterList = list;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listMonster = convertView;
+        if(listMonster == null)
+            listMonster = LayoutInflater.from(mContext).inflate(R.layout.list_monster, parent,false);
+
+        Monster currentForm = MonsterList.get(position);
+
+        TextView name = (TextView) listMonster.findViewById(R.id.monster_name);
+        name.setText(currentForm.getName());
+
+        TextView type_creature = (TextView) listMonster.findViewById(R.id.monster_type_creature);
+        type_creature.setText(currentForm.getType_creature());
+
+        String lvl = "" + currentForm.getLvl();
+        TextView level = (TextView) listMonster.findViewById(R.id.monster_lvl);
+        level.setText(lvl);
+
+        return listMonster;
+    }
+
+}
